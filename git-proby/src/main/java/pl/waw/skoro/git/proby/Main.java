2a4d95a5eb799c93b9ac5fa0b94cf504ea0fb983
Janek Nieznany <janek.nieznany@przyklad.pl>
2016-09-16 19:10:10 +0200

    polskie powitanie oraz dodanie nieuzywanej metody foo

--- git-proby/src/main/java/pl/waw/skoro/git/proby/Main.java
+++ git-proby/src/main/java/pl/waw/skoro/git/proby/Main.java
@@ -1,7 +1,13 @@
 package pl.waw.skoro.git.proby;
 
 public class Main {
+    
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Witaj Świecie!");
+    }
+    
+    @SuppressWarnings("unused")
+    private static void foo() {
+        System.out.println("Unused method");
     }
 }
